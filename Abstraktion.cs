4e994ebaceac565c66f1376c691cd50cc77ad272
danielodiazo-creator <danielodiazo@gmail.com>
2025-09-24 13:50:33 +0200

    I har skapat en abstrakt klass och en subklasss som ärver från denna abstrakta klassen.

---
+++ Abstraktion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oop_Github
+{
+    public abstract class Abstraktion
+    {
+
+        public abstract void Score();
+    }
+
+    public class Competiror : Abstraktion
+    {
+       public override void Score()
+        {
+            Console.WriteLine("Your score is 100/100"); 
+        }
+    }
+
+}
